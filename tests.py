ae3e0637f0533f7ee9c3bbecbed05f2384d4fd06
Posya0 <posya.prozorova.02@gmail.com>
2022-10-04 18:42:07 +0300

    Тесты к функции guess_letter (fixed)

--- tests.py
+++ tests.py
@@ -18,7 +18,7 @@ def test_2():
     else:
         print('test passed')
 
-def test_3():
+def test_3_1():
     try:
         t = guess_letter('word', ['*','*','*','*'], 'o')
         assert t == ['*','o','*','*']
@@ -27,6 +27,27 @@ def test_3():
     else:
         print('test passed')
 
+def test_3_2():
+    try:
+        t = guess_letter('word', ['*','*','*','*'], 'к')
+        assert t == ['*','*','*','*']
+    except:
+        print('test error')
+    else:
+        print('test passed')
+
+def test_3_3():
+    try:
+        t = guess_letter('слово', ['*','*','*','*','*'], 'о')
+        assert t == ['*','*','о','*','о']
+    except:
+        print('test error')
+    else:
+        print('test passed')
+
+
 test_1()
 test_2()
-test_3()
+test_3_1()
+test_3_2()
+test_3_3()
