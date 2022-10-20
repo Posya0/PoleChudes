cfc0d129330b44a0db583c4be11533deed4c4a64
Posya0 <posya.prozorova.02@gmail.com>
2022-10-20 12:33:46 +0300

    Соответствие PEP-8 2

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
+"""поле чудес."""
 import random
 import time
 
 
 def get_question():
-    with open('question.txt', 'r', encoding='utf-8') as f:
-        q_list = f.read().splitlines()
+    """получить вопрос-ответ."""
+    with open('question.txt', 'r', encoding='utf-8') as file:
+        q_list = file.read().splitlines()
     number_q = random.randrange(0, len(q_list))
     q_answer = str(q_list[number_q])
-    for i in range(0, len(q_answer)):
-        if q_answer[i] == ';':
+    for i, ans in enumerate(q_answer):
+        if ans == ';':
             question = q_answer[0:i]
             answer = q_answer[i + 1:len(q_answer)].lower()
     return answer, question
 
 
 def encrypt(answer):
+    """шифровать."""
     curent_view = []
-    for i in range(0, len(answer)):
+    for i in range(len(answer)):
         curent_view.append('*')
     return curent_view
 
 
 def guess_letter(answer, curent_view, letter):
+    """угадать букву."""
     if letter in answer:
         print('Такая буква есть!')
-        for i in range(0, len(answer)):
-            if answer[i] == letter:
+        for i, ans in encrypt(answer):
+            if ans == letter:
                 curent_view[i] = letter
     else:
         print('Такой буквы нет')
@@ -34,40 +38,44 @@ def guess_letter(answer, curent_view, letter):
 
 
 def guess_word(answer, word):
+    """угадать слово."""
     if answer == word:
         return True
-    else:
-        return False
+    return False
 
 
 def drum(score):
-    drum = [100, 200, 300, 400, 500, 600, 1000, 'Банкрот']
-    value = drum[random.randrange(0, len(drum))]
+    """барабан."""
+    drum_list = [100, 200, 300, 400, 500, 600, 1000, 'Банкрот']
+    value = drum_list[random.randrange(0, len(drum_list))]
     if value == 'Банкрот':
         print('Вам выпал банкрот, ваш счет обнулился\n')
         return 0
-    else:
-        print('Вам выпало ' + str(value) + '. Ваш счет: ' + str(score + value) + '\n')
-        return score + value
+    print('Вам выпало ' + str(value) +
+          '. Ваш счет: ' + str(score + value) + '\n')
+    return score + value
 
 
 def compare(curent_view):
-    for i in range(0, len(curent_view)):
+    """проверка."""
+    for i in enumerate(curent_view):
         if curent_view[i] == '*':
             return False
     return True
 
 
 def check_letter(letter):
-    rus_letters = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т",
-                   "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+    """проверка буквы."""
+    rus_letters = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й",
+                   "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф",
+                   "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
     if letter in rus_letters:
         return True
-    else:
-        return False
+    return False
 
 
 def main():
+    """главная функция."""
     answer, question = get_question()
     score = 0
     curent_view = encrypt(answer)
